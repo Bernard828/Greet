5b8c63f3211c59164d1e44dfe2d2a1be642bdbdd
Bernard828 <bernard.lackey1@gmail.com>
2021-07-12 10:53:29 -0400

    created dev branch

--- Greet/Program.cs
+++ Greet/Program.cs
@@ -6,7 +6,9 @@ namespace Greet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello and welcome to the page of wonder." +
+                " This very base of this page was the foundation I used to begin in developing." +
+                " Started from the bottom and now I'm here. My technical skills come from my experience as a lifetime student, and with 9 years of working in the service industry as a server and hotel hospitality.");
         }
     }
 }
